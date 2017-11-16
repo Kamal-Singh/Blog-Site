f032e1aaca0054b52f52cc76c552010ffba2918f
Kamal Singh <ksinghyadav@ymail.com>
2017-11-16 21:22:25 +0530

    Added NEW, CREATE and SHOW RESTful routes also added Semenatics UI

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ mongoose.Promise = global.Promise;
 
 //Create Schema
 var schema = new mongoose.Schema({
-    name: String,
+    title: String,
     image: String,
-    content: String
+    content: String,
+    created: {type: Date, default: Date.now}
 });
 
 var Blog = mongoose.model('Blog', schema);
@@ -26,7 +27,41 @@ app.get('/', function(req, res){
 
 //INDEX route
 app.get('/blogs', function(req,res){
-    res.render('index');
+    Blog.find({}, function(err ,blogs){
+        if(err)
+            {
+                console.log(err);
+            } else {
+                res.render('index', {blogs: blogs});               
+            }
+    })
+});
+
+//NEW Route
+app.get('/blogs/new' ,function(req, res){
+    res.render('new');
+})
+//CREATE route
+app.post('/blogs', function(req, res){
+    Blog.create(req.body.blog, function(err, newblog){
+        if(err)
+            {
+                res.render('new');
+            }
+            else{
+                res.redirect('/blogs');
+            }
+    });
+});
+//SHOW route
+app.get('/blogs/:id', function(req,res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.render('show', {blog: foundBlog});
+        }
+    })
 });
 
 //starting Server on localhost
